4ee302ec98d5048a5f6d67d026bef66627e5c561
MlkAlt <meliksibil@gmail.com>
2026-03-21 16:16:49 +0300

    sw cache fix

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,20 @@
-const CACHE_NAME = 'yillik-plan-v1';
-const urlsToCache = [
-  '/',
-  '/app',
-  '/index.html'
-];
+const CACHE_NAME = 'yillik-plan-v2';
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => caches.delete(cacheName))
+      );
+    })
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
